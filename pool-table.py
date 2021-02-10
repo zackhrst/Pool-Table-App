772ba1c455bf558b9084be30c90aa5e1d3efe4f1
zackhrst <zackhrst@gmail.com>
2021-02-10 14:14:22 -0500

    first commit

---
+++ pool-table.py
@@ -0,0 +1,31 @@
+# Going to need classes. (pool Table)
+# Import date/times
+# User needs to be able to:
+    # -check out table
+    # -check in table
+    # -total time
+# Pool Table: - need empty dictionary of pool tables
+    # Pool table number
+    # Start Date time
+    # End Date time
+    # Total time played
+    # cost - if hard mode
+
+
+class PoolTable:
+        def __init__(self, table_number)
+        self.table_number = table_number
+        self.start_time = none
+
+# creating menu
+while True:
+    print("***Main Menu***")
+    if choice == "1":
+
+    elif choice == "2":
+
+    elif choice = "3":
+    
+
+# Menu
+
