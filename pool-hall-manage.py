c303b910380e47baae903309ce3b17e8202c6156
zackhrst <zackhrst@gmail.com>
2021-02-10 15:02:33 -0500

    Feb 10 - 2 commit

---
+++ pool-hall-manage.py
@@ -0,0 +1,23 @@
+
+def show_menu():
+    # display menu
+print("Reserve a Table: Press 1")
+print("Close out Table: Press 2")
+print("View all Tables: Press 3")
+print("
+
+
+
+
+while True:
+    print("***Main Menu***")
+    print("")
+    if choice == "1":
+
+    elif choice == "2":
+
+    elif choice = "3":
+    
+
+# Menu
+
